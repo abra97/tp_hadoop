39d24572fc8305d391e9c6374e0c045d3b018cb4
Degbun Josue <78054512+degbun@users.noreply.github.com>
2023-06-18 00:31:14 +0000

    pusher sur une branche autre que le main

---
+++ src/main/scala/Jobs/Master1.scala
@@ -0,0 +1,36 @@
+package Jobs
+
+import org.apache.spark.sql.SparkSession
+
+object Master1 extends App {
+  val Job = "job_master"
+
+
+
+  val spark = SparkSession
+
+    .builder()
+    .appName(Job)
+    .enableHiveSupport()
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val dataSeq = Seq(("Java", 20000), ("Python", 100000), ("Scala", 3000))
+
+  val rdd = spark.sparkContext.parallelize(dataSeq)
+
+  println("Nombre initial de partitions :" + rdd.getNumPartitions)
+
+  println("Collect :" + rdd.collect())
+
+  println("Premier element :" + rdd.first())
+
+  val columns = Seq("language", "users_count")
+
+  val dfFromRDD1 = rdd.toDF(columns: _*)
+
+  dfFromRDD1.show
+
+
+}
